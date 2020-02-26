e4301fee9f1915b61a2f130206d6a547b6499690
qorzj <zhangji@parsec.com.cn>
2020-02-26 15:08:07 +0800

    edit index.py

--- leyuan/index.py
+++ leyuan/index.py
@@ -2,10 +2,14 @@ from lesscli import Application
 from leyuan import do_init, do_cli, do_daemon
 
 
-if __name__ == '__main__':
-    Application('leyuan集群管理工具')\
-        .add('init', do_init.app)\
-        .add('block', do_cli.do_block)\
-        .add('exec', do_cli.do_exec)\
-        .add('daemon', do_daemon.do_daemon)\
+def main():
+    Application('leyuan集群管理工具') \
+        .add('init', do_init.app) \
+        .add('block', do_cli.do_block) \
+        .add('exec', do_cli.do_exec) \
+        .add('daemon', do_daemon.do_daemon) \
         .run()
+
+
+if __name__ == '__main__':
+    main()
